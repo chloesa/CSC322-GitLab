3eb67b75b02f23199de24d49fe6b874cf11e6f78
chloesa <117122585+chloesa@users.noreply.github.com>
2022-11-04 15:27:37 -0400

    crazy

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,9 @@
 //-----------------------------------------------------------------------------
-#include <stdio.h>
 //-----------------------------------------------------------------------------
 int main(void) {
 
 <<<<<<< HEAD
-    printf("Hello Very Happy World!\n");
+    printf("Hello Crazy World!\n");
 =======
     printf("Hello Happy World!\n");
 >>>>>>> Happy
